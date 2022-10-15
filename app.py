80861b9d7de7c239ae74b76249cffd7c21e1d365
Krigor Rosa <krigorsilva13@gmail.com>
2022-10-15 18:36:45 -0300

    atualizado

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 from ypstruct import structure
 import AeroGA
@@ -10,7 +9,7 @@ import AeroGA_parallel
 def sphere(x):
     return sum(x**2)
 
-# Gráficos
+# Graphs
 plot_iterations = 1
 plot_box = 0
 
@@ -18,14 +17,14 @@ plot_box = 0
 problem = structure()                                  # Creating the Problem Structure
 problem.fitness = sphere                               # Fitness Function
 problem.nvar = 5                                       # Variables number
-problem.lb = [-10, -10, -10, -5, -5]                  # Lower Bounds
-problem.ub = [ 10, 10, 10,  5, 5]                  # Upper Bounds
-problem.integer = [1,2]                                   # Indice de números inteiros
+problem.lb = [-10, -10, -10, -5, -5]                   # Lower Bounds
+problem.ub = [ 10, 10, 10,  5, 5]                      # Upper Bounds
+problem.integer = [1,2]                                # Indice de números inteiros
 
 # GA Parameters
 params = structure()                                   # Creating the Parameters Structure
 params.max_iterations = 100                            # Number of Max Iterations
-params.npop = 50                                      # Number of the Population
+params.npop = 50                                       # Number of the Population
 params.pc = 1                                          # Proporção da população de filhos em relação aos pais
 params.gamma = 0.1                                     # Amplitude do crossover        
 params.mu = 0.5                                        # Mutation Rate
@@ -36,7 +35,7 @@ params.sigma_int = 0.7                                 # Standart deviation of t
 methods = structure()
 methods.selection = "rank"                             # Available methods: "roulette", "rank", "tournament", "elitism" -> Read README.md for detailed info
 methods.crossover = "arithmetic"                       # Available methods: "arithmetic", "1-point", "2-point" -> Read README.md for detailed info
-methods.mutation = "gaussian"                           # Available methods: "gaussian", "default" -> Read README.md for detailed info
+methods.mutation = "gaussian"                          # Available methods: "gaussian", "default" -> Read README.md for detailed info
 
 
 # Run GA
@@ -47,23 +46,10 @@ out = AeroGA.optimize(problem, params, methods)        # Running the Simulation
 
 # Gráficos - Linear das iterações
 if plot_iterations == 1:
-    # fig = plt.figure()
-    # plt.plot(out.bestfit)
-    # plt.xlim(0, params.max_iterations+1)
-    # plt.xlabel('Iterations')
-    # plt.ylabel('Best Fit')
-    # plt.title('Fitness x Iterations')
-    # plt.grid(True)
     fig1 = out.plots
     plt.show()
 
 # Gráficos - Box plot
 if plot_box == 1:
-    # fig = plt.figure()
-    # plt.boxplot(out.archive_scaled)
-    # plt.xlabel('Variáveis')
-    # plt.ylabel('Valores do GA')
-    # plt.title('Dispersão das Variáveis')
-    # plt.grid(True)
     fig2 = out.plots
     plt.show()
