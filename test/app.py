30842ce646272c6340f509c36fe32999bf97682c
Krigor Rosa <krigorsilva13@gmail.com>
2023-02-17 21:29:34 -0300

    Atualizada a forma como os argumentos da fç optmize são passados + dicas sobre como usar

--- test/app.py
+++ test/app.py
@@ -1,43 +1,37 @@
 import matplotlib.pyplot as plt
 from AeroGA.AeroGA import *
-from ypstruct import structure
 from Benchmarks import *
-# from MDOJunior import MDOJunior
 
 if __name__ == '__main__':
-    # GA Parameters
-    param = structure()
-    param.lb = [0, 0, 0.0, 0.0, 0.0, 0.0]
-    param.ub = [5, 4, 0.4, 1.5, 0.5, 1.0]
-    param.num_variables = 6
-    param.population_size = 20
-    param.num_generations = 100
-    param.eta = 5
-    param.std_dev = 0.1
-    param.elite_count = 1
-    param.online_control = False
-    param.mutation_rate = 0.4
-    param.crossover_rate = 1
+    """ 
+    **********  Dicas de utilização  ***********
 
-    # Fitness function
-    fitness_fn = Rastrigin
+    # Métodos seleção: "roulette", "rank", "tournament" -> Read README.md for detailed info
+    # Métodos crossover: "arithmetic", "SBX" ,"1-point", "2-point" -> Read README.md for detailed info
+    # Métodos mutação: "gaussian", "polynomial" -> Read README.md for detailed info
 
-    # GA Methods
-    methods = structure()
-    methods.selection = "tournament"                       # Available methods: "roulette", "rank", "tournament" -> Read README.md for detailed info
-    methods.crossover = "1-point"                       # Available methods: "arithmetic", "SBX" ,"1-point", "2-point" -> Read README.md for detailed info
-    methods.mutation = "gaussian"                          # Available methods: "gaussian", "polynomial" -> Read README.md for detailed info
-    methods.n_threads = 0                                  # Number of threads used for the fitness calculation
+    *VALORES DEFAULT*
+    out = optimize(selection = "tournament", crossover = "1-point", mutation = "gaussian", n_threads = -1,
+    min_values = [], max_values = [], num_variables = [], population_size = [], num_generations = [], elite_count = [],
+    online_control = False, mutation_rate = 0.4, crossover_rate = 1, eta = 5, std_dev = 0.1,
+    plotfit = True, plotbox = False, plotparallel = False, 
+    fitness_fn = function
 
-    # Plots and Auxiliary functions
-    plot = structure()
-    plot.fit = True
-    plot.parallel = False
-    plot.box = False
+    # mutação gaussiana -> std_dev                      Ou seja não é neceessário definir eta se estiver usando
+    # mutação polinomial -> eta                         mutação gaussiana, para o polinomial serve o mesmo
+       
+    # mutation_rate e crossover_rate só são ativados caso online_control = False
 
+    *VALORES RETORNADOS PELO OUT -> out = [população, history, best_individual, values_gen]
+
+    # Função de sensibilidade -> sensibility(out["best_individual"], fitness_fn, increment=0.01, lb, ub)
+    """
+    
+    lb = [0, 0, 0.0, 0.0, 0.0, 0.0]
+    ub = [5, 0, 0.4, 1.5, 0.5, 1.0]
+    
     # Run the genetic algorithm
-    out = optimize(methods, param, plot, fitness_fn) # out = [população, history, best_individual, values_gen]
-
-    # create_boxplots(out["history"])
-    # parallel_coordinates(out["history"])
-    # sensibility(best_individual, fitness_fn, increment=0.01, min_values, max_values)
+    out = optimize(selection = "tournament", crossover = "1-point", mutation = "gaussian", n_threads=0,
+    min_values = lb, max_values = ub, num_variables = 6, population_size = 50, num_generations = 100, elite_count = 0,
+    fitness_fn = Rastrigin
+    )
