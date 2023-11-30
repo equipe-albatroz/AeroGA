2f65458bf9cdfd898f603391bd6a0fbaa934e491
Krigor Rosa <krigorsilva13@gmail.com>
2023-11-30 12:31:55 -0300

    update

--- test/app.py
+++ test/app.py
@@ -6,6 +6,10 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
 
+    micro = ['c1', 'chord_ratio2','b1','span_ratio2','iw','nperfilw1','nperfilw2','zwGround','xCG','vh', 'ih','nperfilh','motorindex']
+    regular = ['b1', 'span_ratio_2', 'span_ratio_b3', 'c1', 'chord_ratio_c2', 'chord_ratio_c3', 'nperfilw1', 'nperfilw2', 'nperfilw3', 'iw', 'zwground', 'xCG', 'Vh', 'ARh', 'nperfilh', 'lt', 'it', 'xTDP', 'AtivaProfundor', 'motorIndex']
+
+
     lb = [0, 0, 0.0, 0.0, 0.0, 0.0, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0]
     ub = [5, 0, 0.4, 1.5, 0.5, 1.0, 5, 0, 0.4, 1.5, 0.5, 1.0, 1.0]
     
@@ -14,8 +18,12 @@ if __name__ == '__main__':
         min_values = lb, max_values = ub, num_generations = 15, elite_count = 2, plotfit=False,
         fitness_fn = Rastrigin, report=True)
 
-# path = "Resultados\Results_03-03-2023_17-50.xlsx"
-# create_boxplots_por_gen_import_xlsx(path, lb, ub, n_gen = 10, gen = 5)
-# parallel_coordinates_import_xlsx(path,nvar = 13, classe = "micro")
+
+# path = "Resultados\Results_08-07-2023_19-48.xlsx"
+# lb = [0, 0, 0.0, 0.0, 0.0, 0.0]
+# ub = [5, 0, 0.4, 1.5, 0.5, 1.0]
+# var_names = [f'Var_{i+1}' for i in range(6)]; var_names.append('Score')
+# parallel_coordinates_import_xlsx(path, lb, ub, var_names)
+# parallel_coordinates_per_gen_import_xlsx(path, lb, ub, 1, var_names)
 # create_boxplots_import_xlsx(path)
-# create_boxplots_por_gen_import_xlsx(path, 100, 80)
+# create_boxplots_por_gen_import_xlsx(path, lb, ub, 2)
