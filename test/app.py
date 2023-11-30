dde883d9a07719922369b360a6360ee129182428
Krigor Rosa <krigorsilva13@gmail.com>
2023-11-29 23:54:57 -0300

    update

--- test/app.py
+++ test/app.py
@@ -6,16 +6,12 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
 
-    # lb = [0, 0, 0.0, 0.0, 0.0, 0.0, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    # ub = [5, 0, 0.4, 1.5, 0.5, 1.0, 5, 0, 0.4, 1.5, 0.5, 1.0, 1.0, 5, 0, 0.4, 1.5, 0.5, 1.0, 1.0]
-    # lb = [0, 0, 0.0, 0.0, 0.0, 0.0, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    # ub = [5, 0, 0.4, 1.5, 0.5, 1.0, 5, 0, 0.4, 1.5, 0.5, 1.0, 1.0]
-    lb = [0, 0, 0.0, 0.0, 0.0, 0.0]
-    ub = [5, 0, 0.4, 1.5, 0.5, 1.0]
+    lb = [0, 0, 0.0, 0.0, 0.0, 0.0, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    ub = [5, 0, 0.4, 1.5, 0.5, 1.0, 5, 0, 0.4, 1.5, 0.5, 1.0, 1.0]
     
     # Run the genetic algorithm
     out = optimize(selection = "tournament", crossover = "1-point", mutation = "polynomial", n_threads=-1,
-        min_values = lb, max_values = ub, num_variables = 6, num_generations = 2, elite_count = 2, plotfit=False,
+        min_values = lb, max_values = ub, num_generations = 15, elite_count = 2, plotfit=False,
         fitness_fn = Rastrigin, report=True)
 
 # path = "Resultados\Results_03-03-2023_17-50.xlsx"
