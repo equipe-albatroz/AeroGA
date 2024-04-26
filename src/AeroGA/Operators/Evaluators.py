fe51850df1175f7f28e147adc73db6c7445c5169
Krigor Rosa <krigorsilva13@gmail.com>
2024-04-26 01:16:42 -0300

    the parallel fit calculation is now made with joblib library, to ensure all threads selected are used.

--- src/AeroGA/Operators/Evaluators.py
+++ src/AeroGA/Operators/Evaluators.py
@@ -4,19 +4,31 @@ Functions dedicated to calculating the fitness of individuals in the population.
 
 import multiprocessing
 from AeroGA.Classes.Error import ErrorType, Log
+from joblib import Parallel, delayed
 
 # Setting error log file
 ErrorLog = Log("error.log", 'Evaluators')
 
 def parallel_fitness(population = list, fitness_fn = None, num_processes = int):
-    """Calculate the fitness of each individual in the population."""
+    if num_processes == -1:
+        num_processes = multiprocessing.cpu_count()
+
     try:
-        with multiprocessing.Pool(num_processes) as pool:
-            fitness_values = pool.map(fitness_fn, population)
+        fitness_values = Parallel(n_jobs=num_processes)(delayed(fitness_fn)(individual) for individual in population)
         return fitness_values
     except Exception as e:
         ErrorLog.error(str(e))
-        return ErrorType("danger", str(e), 'parallel_fitness')
+        return ErrorType("danger", str(e), 'parallel_fitness')   
+
+# def parallel_fitness(population = list, fitness_fn = None, num_processes = int):
+#     """Calculate the fitness of each individual in the population."""
+#     try:
+#         with multiprocessing.Pool(num_processes) as pool:
+#             fitness_values = pool.map(fitness_fn, population)
+#         return fitness_values
+#     except Exception as e:
+#         ErrorLog.error(str(e))
+#         return ErrorType("danger", str(e), 'parallel_fitness')
 
 # Fitness function without using multi threads
 def fitness(population = list, fitness_fn = None):
